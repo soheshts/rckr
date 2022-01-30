f13d3e2f32ff764afcdf0e8955e5c635a24b6653
Sohesh TS <soheshts@gmail.com>
2022-01-30 23:00:33 +0530

    currency filter changes

--- src/main/java/io/github/soheshts/App.java
+++ src/main/java/io/github/soheshts/App.java
@@ -3,41 +3,73 @@ package io.github.soheshts;
 import io.github.soheshts.model.Country;
 import io.github.soheshts.util.Constants;
 import io.github.soheshts.util.Util;
+import org.apache.commons.math3.util.Precision;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Hello world!
  */
 public class App {
-    private static final Long populationLimit = 65110000L;
+    private static final Long populationLimit = 6700L;
 
     public static void main(String[] args) {
         List<Country> countryList;
-        List<Country> filteredList;
+        List<Country> populationList;
+        List<Country> oneCurrencyList;
+        List<Country> manyCurrencyList;
+        List<Country> finalList = new ArrayList<>();
+        Set<String> yesCurrencySet = new HashSet<>();
+        Set<String> noCurrencySet = new HashSet<>();
 //        BigDecimal distance = new BigDecimal(0);
         double distance = 0.0;
         try {
             System.out.println("Getting CountryList");
             countryList = Util.readJsonFromUrl(Constants.jsonURL);
+//            Find repeated currencies i.e used by other countries
+            for (Country country : countryList) {
+                for (int i = 0; i < country.getCurrencies().size(); i++) {
+
+                    if (!yesCurrencySet.contains(country.getCurrencies().get(i).getName())) {
+                        yesCurrencySet.add(country.getCurrencies().get(i).getName());
+                    } else {
+                        noCurrencySet.add(country.getCurrencies().get(i).getName());
+                    }
+                }
+            }
             System.out.println("Got CountryList");
-            filteredList = countryList.stream().filter(country -> country.getCurrencies().size() > 0
-                    && country.getPopulation() >= populationLimit).collect(Collectors.toList());
-            Collections.sort(filteredList, (c1, c2) -> c1.getPopulation().compareTo(c2.getPopulation()));
-            Collections.reverse(filteredList);
-            filteredList.subList(20, filteredList.size()).clear();
-            System.out.println(filteredList.toString());
-            for (int i = 0; i < filteredList.size() - 1; i++) {
-                Country c1 = filteredList.get(i);
-                Country c2 = filteredList.get(i + 1);
-                System.out.print(c1.getName() +"( "+c1.getLatlng()[0]+" , "+ c1.getLatlng()[1]+" )"
-                        + " and " + c2.getName() +"( "+c2.getLatlng()[0]+" , "+ c2.getLatlng()[1]+" )"  + " : ");
+            populationList = countryList.stream().filter(country -> country.getPopulation() >= populationLimit).collect(Collectors.toList());
+            oneCurrencyList = populationList.stream().filter(country -> country.getCurrencies().size() == 1).collect(Collectors.toList());
+            manyCurrencyList = populationList.stream().filter(country -> country.getCurrencies().size() > 1).collect(Collectors.toList());
+            for (Country country : oneCurrencyList) {
+                if (!noCurrencySet.contains(country.getCurrencies().get(0).getName())) {
+                    finalList.add(country);
+                }
+            }
+            for (Country country : manyCurrencyList) {
+                for (int i = 0; i < country.getCurrencies().size(); i++) {
+
+                    if (!noCurrencySet.contains(country.getCurrencies().get(i).getName())) {
+                        finalList.add(country);
+                    }
+                }
+            }
+            Collections.sort(finalList, (c1, c2) -> c1.getPopulation().compareTo(c2.getPopulation()));
+
+//            Collections.reverse(populationList);
+            finalList.subList(20, finalList.size()).clear();
+            System.out.println(finalList.toString());
+            for (int i = 0; i < finalList.size() - 1; i++) {
+                Country c1 = finalList.get(i);
+                Country c2 = finalList.get(i + 1);
+                System.out.print(c1.getName() + "( " + c1.getLatlng()[0] + " , " + c1.getLatlng()[1] + " )"
+                        + " and " + c2.getName() + "( " + c2.getLatlng()[0] + " , " + c2.getLatlng()[1] + " )" + " : ");
                 distance = distance + Util.distance(c1.getLatlng()[0], c2.getLatlng()[0], c1.getLatlng()[1],
                         c2.getLatlng()[1]);
+
             }
-            System.out.println("Total: " + distance);
+            System.out.println("Total: " + Precision.round(distance, 2));
         } catch (Exception e) {
             System.out.println(e);
         }
--- src/main/java/io/github/soheshts/util/Util.java
+++ src/main/java/io/github/soheshts/util/Util.java
@@ -3,12 +3,12 @@ package io.github.soheshts.util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import io.github.soheshts.model.Country;
+import org.apache.commons.math3.util.Precision;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -26,46 +26,10 @@ public class Util {
 
             return countries;
         }
-
-
     }
 
-    /**
-     * Calculate distance between two points in latitude and longitude taking
-     * into account height difference. If you are not interested in height
-     * difference pass 0.0. Uses Haversine method as its base.
-     * <p>
-     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
-     * el2 End altitude in meters
-     *
-     * @returns Distance in Meters
-     *//*
-    public static BigDecimal distance(double lat1, double lat2, double lon1,
-                                      double lon2, double el1, double el2) {
-
-        final int R = 6371; // Radius of the earth
-
-        double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lon2 - lon1);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000; // convert to meters
-
-        double height = el1 - el2;
-
-        distance = Math.pow(distance, 2) + Math.pow(height, 2);
-        double roundOff = (double) Math.round(Math.sqrt(distance) * 100) / 100;
-        System.out.println( roundOff);
-
-        return new BigDecimal(roundOff);
-    }*/
-
-    public static double distance(double lat1,
-                                  double lat2, double lon1,
-                                  double lon2)
-    {
+    public static double distance(double lat1, double lat2, double lon1,
+                                  double lon2) {
 
         // The math module contains a function
         // named toRadians which converts from
@@ -80,17 +44,17 @@ public class Util {
         double dlat = lat2 - lat1;
         double a = Math.pow(Math.sin(dlat / 2), 2)
                 + Math.cos(lat1) * Math.cos(lat2)
-                * Math.pow(Math.sin(dlon / 2),2);
+                * Math.pow(Math.sin(dlon / 2), 2);
 
         double c = 2 * Math.asin(Math.sqrt(a));
 
         // Radius of earth in kilometers. Use 3956
         // for miles
         double r = 6371;
-        double roundOff = (double) Math.round((c*r) * 100) / 100;
-        System.out.println(roundOff);
+
+        System.out.println(Precision.round(c*r,2));
         // calculate the result
-        return(c * r);
+        return Precision.round(c*r,2);
     }
 
 }
