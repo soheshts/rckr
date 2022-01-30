0d4cf85e8bf9ba901f9ef86fe8f36d41d4eaa786
Sohesh TS <soheshts@gmail.com>
2022-01-30 13:48:14 +0530

    Initial commit

---
+++ src/main/java/io/github/soheshts/App.java
@@ -0,0 +1,13 @@
+package io.github.soheshts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
